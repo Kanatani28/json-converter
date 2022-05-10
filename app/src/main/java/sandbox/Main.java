d2e0c741287631d57e3427570b1730b2efd3f564
maroKanatani <16130443+Kanatani28@users.noreply.github.com>
2022-05-10 16:52:33 +0900

    add sample files

--- app/src/main/java/sandbox/Main.java
+++ app/src/main/java/sandbox/Main.java
@@ -4,13 +4,17 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String jsonPath = "src/main/resources/sample.json";
-        // String jsonPath = "src/main/resources/sample2.json";
+        String jsonPath1 = "src/main/resources/sample.json";
+        String jsonPath2 = "src/main/resources/sample2.json";
+        String jsonPath3 = "src/main/resources/sample3.json";
+
         String outputDir = "src/main/resources/";
 
-        JsonConverter.convertToCsv(jsonPath, outputDir);
-        // JsonConverter2.convertToCsv(jsonPath, outputDir);  
-        // JsonConverter3.convertToCsv(jsonPath, outputDir);  
-        // JsonConverter4.convertToCsv(jsonPath, outputDir);  
+        JsonConverter.convertToCsv(jsonPath1, outputDir);
+
+        JsonConverter.convertToCsv(jsonPath2, outputDir);
+        // JsonConverter2.convertToCsv(jsonPath1, outputDir);  
+        // JsonConverter3.convertToCsv(jsonPath1, outputDir);  
+        // JsonConverter4.convertToCsv(jsonPath1, outputDir);  
     }
 }
